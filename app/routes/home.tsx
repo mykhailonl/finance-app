08451c4cd65089e8a1517507753faccda6372fc9
Mykhailo Lapchynskyi <mikhaillapchinskyi@gmail.com>
2025-07-21 14:25:00 +0200

    Initial commit with React Router v7

---
+++ app/routes/home.tsx
@@ -0,0 +1,11 @@
+export async function loader() {
+}
+
+export default function Index() {
+  return (
+    <div className="p-8">
+      <h1 className="text-preset-1 text-grey-900">Finance App</h1>
+      <p className="text-preset-4 text-grey-500 mt-4">Welcome to your personal finance dashboard.</p>
+    </div>
+  );
+}
