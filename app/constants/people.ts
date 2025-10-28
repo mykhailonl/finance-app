432504b716f4dffb8ac39f4e6ea9da20fc04a918
Mykhailo Lapchynskyi <mikhaillapchinskyi@gmail.com>
2025-10-28 14:57:00 +0100

    updated people.ts, copied icons to public folder

--- app/constants/people.ts
+++ app/constants/people.ts
@@ -1,21 +1,21 @@
 import type { PersonName } from '~/types'
 
 export const peopleAvatars: Record<PersonName, string> = {
-  Emma: '/app/assets/images/avatars/emma-richardson.jpg',
-  Daniel: '/app/assets/images/avatars/daniel-carter.jpg',
-  Ella: '/app/assets/images/avatars/ella-phillips.jpg',
-  Ethan: '/app/assets/images/avatars/ethan-clark.jpg',
-  Harper: '/app/assets/images/avatars/harper-edwards.jpg',
-  James: '/app/assets/images/avatars/james-thompson.jpg',
-  Liam: '/app/assets/images/avatars/liam-hughes.jpg',
-  Lily: '/app/assets/images/avatars/lily-ramirez.jpg',
-  Mason: '/app/assets/images/avatars/mason-martinez.jpg',
-  Rina: '/app/assets/images/avatars/rina-sato.jpg',
-  Sebastian: '/app/assets/images/avatars/sebastian-cook.jpg',
-  Sofia: '/app/assets/images/avatars/sofia-peterson.jpg',
-  Sun: '/app/assets/images/avatars/sun-park.jpg',
-  William: '/app/assets/images/avatars/william-harris.jpg',
-  Yuna: '/app/assets/images/avatars/yuna-kim.jpg',
+  Emma: '/assets/images/avatars/emma-richardson.jpg',
+  Daniel: '/assets/images/avatars/daniel-carter.jpg',
+  Ella: '/assets/images/avatars/ella-phillips.jpg',
+  Ethan: '/assets/images/avatars/ethan-clark.jpg',
+  Harper: '/assets/images/avatars/harper-edwards.jpg',
+  James: '/assets/images/avatars/james-thompson.jpg',
+  Liam: '/assets/images/avatars/liam-hughes.jpg',
+  Lily: '/assets/images/avatars/lily-ramirez.jpg',
+  Mason: '/assets/images/avatars/mason-martinez.jpg',
+  Rina: '/assets/images/avatars/rina-sato.jpg',
+  Sebastian: '/assets/images/avatars/sebastian-cook.jpg',
+  Sofia: '/assets/images/avatars/sofia-peterson.jpg',
+  Sun: '/assets/images/avatars/sun-park.jpg',
+  William: '/assets/images/avatars/william-harris.jpg',
+  Yuna: '/assets/images/avatars/yuna-kim.jpg',
 }
 
 export type AvatarType = {
